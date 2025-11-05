4d8b6f746f73e62522caf75cb31a8dc281423778
[DarlinHolguin] <[darlinholguin123@gmail.com]>
2025-11-04 23:42:44 -0500

    8 Passed

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,20 +1,54 @@
 const makeIdFunc = () => {
-
+  let maker = 0;
+  const idMaker = () => {
+    maker++
+    return maker
+  }
+  return idMaker
 };
+
+
 
 const makePasswordChecker = (correctPassword) => {
+  let guessTimes = 0
+  return (guess) => {
+
+    if (guessTimes === 3) {
+      return 'Account locked'
+    }
+    if (guess === correctPassword) {
+      guessTimes++
+      return true
+    } else {
+      guessTimes++
+      return false
+    }
+  }
 
 };
+
 
 const makeMultiplier = (multiplier) => {
+  return (arrOfNum) => {
+    return arrOfNum.map(num => num * multiplier)
+  }
 
 };
 
-const makeFilterByLength = (length) => {
+const makeFilterByLength = (maxLength) => {
+  return (arrOfStrings) => {
+    return arrOfStrings.filter(string => string.length <= maxLength)
 
+  }
 };
 
 const makeGradeTracker = () => {
+  const grades = []
+  return students = {
+    name:
+
+  }
+
 
 };
 
